48c6d8b4aa0468e955c817e408a7f6ecf2a7d9ce
Park-JuHyeon <j-hyeon97@naver.com>
2023-02-02 15:01:58 +0900

    Update code22_person.py

--- Day04/code22_person.py
+++ Day04/code22_person.py
@@ -1,13 +1,26 @@
 # 클래스 생성
-class Person:    
+class Person:     # 클래스 정의할때는 ()없음
    # pass   person에 넣을게딱히없을때 pass를 사용
     name = '익명'
     height = ''
     gender = ''
     blood_type = 'A'
 
+    # 1. 생성자 추가
+    # def __init__(self):
+    #    self.name = '홍길동'
+    #   self.height = '170'
+    #    self.gender = 'male'
+    #    self.blood_type = 'ABO'
+
+    def __init__(self, name = '홍길동', height = 170 , gender = 'male') -> None:
+        self.name = name
+        self.height = height
+        self.gender = gender
+
+
     def walk(self):   # 클래스에서 self는 항상 들어가야한다 무조건!
-        print('걷습니다')
+        print(f'{self.name}이(가) 걷습니다')
 
     def run(self, option):
         if option == 'Fast':
@@ -17,20 +30,28 @@ class Person:
             print(f'{self.name}이(가) 천천히 뜁니다.')
 
     def stop(self):
-        print('멈춥니다.')
+        print(f'{self.name}이(가) 멈춥니다.')
 
-JuHyeon = Person()   # 이러한 형태의 객체를 instance라고 함.
-JuHyeon.name = '박주현'
-JuHyeon.height = '170'
-JuHyeon.gender = 'male'
-JuHyeon.blood_type = 'A'
+JuHyeon = Person('박주현', 170, 'male')   # 이러한 형태의 객체생성: instance라고 함. 클래스를 사용할때는 ()존재함
+#JuHyeon.name = '박주현'
+#JuHyeon.height = '170'
+#JuHyeon.gender = 'male'
+#JuHyeon.blood_type = 'A'
 
 print(f'{JuHyeon.name}의 혈액형은 {JuHyeon.blood_type}형 입니다.')
 
 JuHyeon.run('Fast')
 JuHyeon.stop()
 
+# 1.초기화 후에 객체생성
+hong = Person()
+hong.run('slow')
 
-
+print('======================')
+# 2. 파라미터를 받는 생성자 실행
+ashely = Person('애슐리', 165, 'female')
+print(ashely.name)
+print(ashely.height)
+print(ashely.gender)
 
 
